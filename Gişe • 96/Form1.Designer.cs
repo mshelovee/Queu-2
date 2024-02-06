b59bcf68eb67d5b68eb0184480594c501805c7ed
halley <130781977+mshelovee@users.noreply.github.com>
2024-02-06 12:08:43 +0300

    Add files via upload

---
+++ Gişe • 96/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Gişe___96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.btnGetir1 = new System.Windows.Forms.Button();
+            this.btnGetir2 = new System.Windows.Forms.Button();
+            this.btnGetir3 = new System.Windows.Forms.Button();
+            this.lblGise1 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblGise2 = new System.Windows.Forms.Label();
+            this.lblGise3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(41, 204);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(307, 20);
+            this.txtAd.TabIndex = 0;
+            // 
+            // btnGetir1
+            // 
+            this.btnGetir1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGetir1.Location = new System.Drawing.Point(6, 34);
+            this.btnGetir1.Name = "btnGetir1";
+            this.btnGetir1.Size = new System.Drawing.Size(74, 38);
+            this.btnGetir1.TabIndex = 1;
+            this.btnGetir1.Text = "1";
+            this.btnGetir1.UseVisualStyleBackColor = false;
+            this.btnGetir1.Click += new System.EventHandler(this.btnGetir1_Click);
+            // 
+            // btnGetir2
+            // 
+            this.btnGetir2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGetir2.Location = new System.Drawing.Point(118, 34);
+            this.btnGetir2.Name = "btnGetir2";
+            this.btnGetir2.Size = new System.Drawing.Size(74, 38);
+            this.btnGetir2.TabIndex = 2;
+            this.btnGetir2.Text = "2";
+            this.btnGetir2.UseVisualStyleBackColor = false;
+            this.btnGetir2.Click += new System.EventHandler(this.btnGetir2_Click);
+            // 
+            // btnGetir3
+            // 
+            this.btnGetir3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGetir3.Location = new System.Drawing.Point(226, 34);
+            this.btnGetir3.Name = "btnGetir3";
+            this.btnGetir3.Size = new System.Drawing.Size(75, 38);
+            this.btnGetir3.TabIndex = 3;
+            this.btnGetir3.Text = "3";
+            this.btnGetir3.UseVisualStyleBackColor = false;
+            this.btnGetir3.Click += new System.EventHandler(this.btnGetir3_Click);
+            // 
+            // lblGise1
+            // 
+            this.lblGise1.AutoSize = true;
+            this.lblGise1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGise1.Location = new System.Drawing.Point(69, 134);
+            this.lblGise1.Name = "lblGise1";
+            this.lblGise1.Size = new System.Drawing.Size(18, 20);
+            this.lblGise1.TabIndex = 4;
+            this.lblGise1.Text = "1";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(41, 230);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(307, 51);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "SIRA AL";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGetir1);
+            this.groupBox1.Controls.Add(this.btnGetir2);
+            this.groupBox1.Controls.Add(this.btnGetir3);
+            this.groupBox1.Location = new System.Drawing.Point(41, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 83);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GİŞE";
+            // 
+            // lblGise2
+            // 
+            this.lblGise2.AutoSize = true;
+            this.lblGise2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGise2.Location = new System.Drawing.Point(188, 134);
+            this.lblGise2.Name = "lblGise2";
+            this.lblGise2.Size = new System.Drawing.Size(18, 20);
+            this.lblGise2.TabIndex = 7;
+            this.lblGise2.Text = "2";
+            // 
+            // lblGise3
+            // 
+            this.lblGise3.AutoSize = true;
+            this.lblGise3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGise3.Location = new System.Drawing.Point(298, 134);
+            this.lblGise3.Name = "lblGise3";
+            this.lblGise3.Size = new System.Drawing.Size(18, 20);
+            this.lblGise3.TabIndex = 8;
+            this.lblGise3.Text = "3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(394, 328);
+            this.Controls.Add(this.lblGise3);
+            this.Controls.Add(this.lblGise2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lblGise1);
+            this.Controls.Add(this.txtAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnGetir1;
+        private System.Windows.Forms.Button btnGetir2;
+        private System.Windows.Forms.Button btnGetir3;
+        private System.Windows.Forms.Label lblGise1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblGise2;
+        private System.Windows.Forms.Label lblGise3;
+    }
+}
+
---
+++ Gişe • 96/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gişe___96
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> musteri = new Queue<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void musteriCagir(Label lbl)
+        {
+            try
+            {
+                //hatalı olabilecek kod(lar) burada olur.
+                lbl.Text = musteri.Dequeue();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message); 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            musteri.Enqueue("Nursevim");
+            musteri.Enqueue("Hilal");
+            musteri.Enqueue("Yezda");
+            musteri.Enqueue("Ravza");
+            musteri.Enqueue("İlay");
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            musteri.Enqueue(txtAd.Text);
+        }
+
+        private void btnGetir1_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblGise1);
+        }
+
+        private void btnGetir2_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblGise2);
+        }
+
+        private void btnGetir3_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblGise3);
+        }
+    }
+}
